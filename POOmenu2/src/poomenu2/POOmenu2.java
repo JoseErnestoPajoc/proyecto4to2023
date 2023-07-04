7fe357772f77d5e6edfb457d3061eb4ed1d2bba0
ernestoPajoc <jose.ernesto.pajoc@outlook.com>
2023-07-04 12:45:23 -0600

    Actualizando el menu principal

--- POOmenu2/src/poomenu2/POOmenu2.java
+++ POOmenu2/src/poomenu2/POOmenu2.java
@@ -8,10 +8,11 @@ public class POOmenu2 {
         int op = 0;
         Scanner entrada = new Scanner(System.in);
         
-        while(op!=3){
+        while(op!=4){
             System.out.println("1. Crear profesor");
             System.out.println("2. Ver profesor");
-            System.out.println("3. Salir");
+            System.out.println("3. Actualizar datos del profesor");
+            System.out.println("4. Salir");
             System.out.println("Ingrese el número de opcion");
             op = entrada.nextInt();
             if(op == 1){

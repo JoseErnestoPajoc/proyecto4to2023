ead334afd686a7b56999b0fa3ae88c2363430e09
ernestoPajoc <jose.ernesto.pajoc@outlook.com>
2023-07-04 12:38:05 -0600

    subiendo el proyecto a github

---
+++ POOmenu2/src/poomenu2/POOmenu2.java
@@ -0,0 +1,29 @@
+package poomenu2;
+
+import java.util.Scanner;
+
+public class POOmenu2 {
+
+    public static void main(String[] args) {
+        int op = 0;
+        Scanner entrada = new Scanner(System.in);
+        
+        while(op!=3){
+            System.out.println("1. Crear profesor");
+            System.out.println("2. Ver profesor");
+            System.out.println("3. Salir");
+            System.out.println("Ingrese el número de opcion");
+            op = entrada.nextInt();
+            if(op == 1){
+                System.out.println("------Nuevo profesor-------");
+            }else if(op == 2){
+                System.out.println("--------Los datos son------");
+            }else if(op == 3){
+                System.out.println("Programa finalizado");
+            }else{
+                System.out.println("Opcion incorrecta");
+            }
+        }
+    }
+    
+}
---
+++ POOmenu2/src/poomenu2/profesor.java
@@ -0,0 +1,11 @@
+package poomenu2;
+
+public class profesor {
+    //atributos
+    private String nombre, materia;
+    
+    public profesor(String nombre, String materia){
+        this.nombre = nombre;
+        this.materia = materia;
+    }
+}
